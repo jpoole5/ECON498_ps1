ff5e117738aca47dce4dcadc7d4fad640cbf361e
Jackson <jpoole5@g.clemson.edu>
2019-02-24 17:07:12 -0500

    first commit

---
+++ HW Scrapper.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 16:55:15 2019
+
+@author: Jackson
+"""
+
+import urllib.request
+import os
+import time
+import datetime
+
+if not os.path.exists(")
