a1da338d09f8f5f11586bc90ba0abf41407b1c06
Jackson <jpoole5@g.clemson.edu>
2019-04-14 17:01:27 -0400

    Finding HTML Patterns

--- HW1_Parsing.py
+++ HW1_Parsing.py
@@ -10,3 +10,25 @@ from bs4 import BeautifulSoup
 import os
 import glob
 
+if not os.path.exists("parsed_files"):
+	os.mkdir("parsed_files")
+    
+pd = pd.dataframe()
+
+for file in glob.glob(html_files/*.html):
+    print("parsed " + file)
+    f = open(file, "r")
+    scrapping_time = os.path.splitext(os.path.basename(file))[0].replace("coinmarketcap","")
+    soup = BeautifulSoup(f.read(),'html parser')
+    f.close
+        #table id = currencies-all
+        #then find all tbody
+        #then find all tr
+        
+        
+        
+        for r in currency_rows:
+            
+            
+        
+        
--- coinmarketcap_request.py
+++ coinmarketcap_request.py
@@ -10,22 +10,17 @@ import os
 import time
 import datetime
 
-if not os.path.exists("html_files_hw"):
-    os.mkdir("html_files_hw")
-    #makes html folder
-    
-
+if not os.path.exists("html_files"):
+	os.mkdir("html_files")
 
 for i in range(24):
-    current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
-    print(str(i) + ": " + current_time_stamp)
-    f = open("html_files_hw/coinmarketcap.html" + current_time_stamp +".html", "wb")
-    response = urllib.request.urlopen('https://coinmarketcap.com/all/views/all/')
-    html = response.read()
-    print (html)
-    f.write(html)
-    f.close()
-    print ("requesting coin market cap")
-    time.sleep(7100)
+	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
+	print(str(i) + ": " + current_time_stamp)
+	f = open("html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+	response = urllib.request.urlopen('https://coinmarketcap.com/all/views/all/')
+	html = response.read()
+	f.write(html)
+	f.close()
+	time.sleep(7200)
     
 #possible to add a ton of different variations to make website think you are human (won't get banned)
