e02affacdb37d590d2e7d7d9c7dd913f83553911
Jackson <jpoole5@g.clemson.edu>
2019-04-14 17:38:20 -0400

    syntax corrections, working

--- HW1_Parsing.py
+++ HW1_Parsing.py
@@ -5,7 +5,7 @@ Created on Mon Apr  8 20:37:58 2019
 @author: Jackson
 """
 
-from pandas as pd
+import pandas as pd
 from bs4 import BeautifulSoup
 import os
 import glob
@@ -13,21 +13,21 @@ import glob
 if not os.path.exists("parsed_files"):
 	os.mkdir("parsed_files")
     
-pd = pd.dataframe()
+df = pd.DataFrame()
 
-for file in glob.glob(html_files/*.html):
-    print("parsed " + file)
-    f = open(file, "r")
+for file in glob.glob("html_files/*.html"):
+    print("parsing " + file)
+    f = open(file, "r", encoding="utf-8")
     scrapping_time = os.path.splitext(os.path.basename(file))[0].replace("coinmarketcap","")
-    soup = BeautifulSoup(f.read(),'html parser')
+    soup = BeautifulSoup(f.read(),'html.parser')
     f.close
-        #table id = currencies-all
-        #then find all tbody
-        #then find all tr
-        table = soup.find("table" {"id": "currencies-all"})
-        tbody = table.find("tbody")
-        currency_rows = tbody.find_all("tr")
-        for r in currency_rows:
+    #table id = currencies-all
+    #then find all tbody
+    #then find all tr
+    table = soup.find("table", {"id": "currencies-all"})
+    tbody = table.find("tbody")
+    currency_rows = tbody.find_all("tr")
+    for r in currency_rows:
             symbol = r.find("td", {"class": "currency-name"}).find("span",{"class":"currency-symbol"}).find("a").text
             name = r.find("td", {"class": "currency-name"}).find("a",{"class":"currency-name-container"}).text
             market_cap = r.find("td", {"class": "market-cap"})['data-sort']
@@ -35,7 +35,7 @@ for file in glob.glob(html_files/*.html):
             supply = r.find("td", {"class": "circulating-supply"})['data-sort']
             volume = r.find("a",{"class": "volume"}).text
             
-df.to_csv("parsed_files/HW1_Parsed_Dataset)
+df.to_csv("parsed_files/HW1_Parsed_Dataset.csv")
            
             
                 
